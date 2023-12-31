d5910967d5df50df59656dc3c0ab0eb099efec08
karimyasser107 <karimyasser34@gmail.com>
2023-12-31 20:10:34 +0200

    preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,15 +1,10 @@
-# %%
 from utils import *
-
-# %%
 import nltk
 import string
 import re
 import pickle
 from nltk.tokenize import sent_tokenize, word_tokenize
-# nltk.download('punkt')
 
-# %%
 # read the training set
 def readFile(file_path):
     # Open the file for reading
@@ -17,25 +12,29 @@ def readFile(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
         # Read the content of the file
         file_content = file.read()
-    return clear_english_and_numbers(file_content)
+    return (file_content)
 
-# %%
-def get_sentences(data):
-    #return nltk.tokenize.sent_tokenize(data) #sent_tokenize(data.strip())
-    sentences = re.split("[\n.؛،]+", data)
-    # sentences = [s for sentence in senten]
+def cleanText(data):
+    punctuations=["،","؛","؟",".",",","!",":"," "]
+    for char in data:
+        #check if char is not in arabic alphabet or harakat
+        if char not in (tuple(list(harakat)+list(arabic_alphabet.keys())+punctuations)):
+            data = data.replace(char,'')
+    # split text into sentences using regex
+    
+    data = re.sub('  +',' ', data)
+    
+    data = re.split(r'[،؛؟.,!:]+', data)
+    
+    return data
+def get_sentences(sentences):
     words_per_sentence = []
     tashkeel_per_sentence = []
     for sentence in sentences:
+        sentence = sentence.strip()
         if sentence == '': continue # new line may be followed by full stop
-        sentence = clear_punctuations(sentence)
-        # tashkel, length = get_tashkel(sentence)
-        # sentence = clear_tashkel(sentence)
-        sentence = word_tokenize(sentence.strip())
+        sentence = word_tokenize(sentence)
         words_per_sentence.append(sentence)
-        # temp_tashkeel = []
-        # for word in sentence:
-        #     temp_tashkeel.append(get_tashkel(word))
     
     tashkeel_per_sentence = [[None for _ in w ]for w in words_per_sentence]
     for i, x in enumerate(words_per_sentence):
@@ -43,28 +42,18 @@ def get_sentences(data):
             tashkeel = get_tashkel(y)
             words_per_sentence[i][j] = clear_tashkel(y)
             tashkeel_per_sentence[i][j] = tashkeel
-    # return [sent for sentence in sif line for sent in sent_tokenize(line.strip()) if sent]
-    #return [sent for line in data.split('\n') if line for sent in sent_tokenize(line) if sent]
     return words_per_sentence, tashkeel_per_sentence
 
-sent="عَُمرً"
+sent=["عَُمرً"] 
 
 print(get_sentences(sent))
-# words_per_sentence, tashkeel_per_sentence=get_sentences("بَُ")
-# print(ord(tashkeel_per_sentence[0][0][1]))
 
-# %%
-# words_without_diacritics = clear_tashkel(file_content)
-
-# %%
-# test sentence
 test_sentence = '''
 قَوْلُهُ : ( أَوْ قَطَعَ الْأَوَّلُ يَدَهُ إلَخْ ) قَالَ الزَّرْكَشِيُّ( 14 / 123 )
 ابْنُ عَرَفَةَ : قَوْلُهُ : بِلَفْظٍ يَقْتَضِيه كَإِنْكَارِ غَيْرِ حَدِيثٍ بِالْإِسْلَامِ وُجُوبَ مَا عُلِمَ وُجُوبُهُ مِنْ الدِّينِ ضَرُورَةً ( كَإِلْقَاءِ مُصْحَفٍ بِقَذَرٍ وَشَدِّ زُنَّارٍ ) ابْنُ عَرَفَةَ : قَوْلُ ابْنِ شَاسٍ : أَوْ بِفِعْلٍ يَتَضَمَّنُهُ هُوَ كَلُبْسِ الزُّنَّارِ وَإِلْقَاءِ الْمُصْحَفِ فِي صَرِيحِ النَّجَاسَةِ وَالسُّجُودِ لِلصَّنَمِ وَنَحْوِ ذَلِكَ ( وَسِحْرٍ ) مُحَمَّدٌ : قَوْلُ مَالِكٍ وَأَصْحَابِهِ أَنَّ السَّاحِرَ كَافِرٌ بِاَللَّهِ تَعَالَى قَالَ مَالِكٌ : هُوَ كَالزِّنْدِيقِ إذَا عَمِلَ السِّحْرَ بِنَفْسِهِ قُتِلَ وَلَمْ يُسْتَتَبْ .
 '''
 
-# %%
-# dataset_without_diacritic, diacritics = get_sentences(clear_english_and_numbers(test_sentence))
+
 
 
 # %%
@@ -77,8 +66,6 @@ def makeWord2VecModel(dataset):
     return word2vec_model
 
 
-# %%
-import numpy
 
 def getCharacterEncoding(word):
     word_embedding = list()
@@ -86,6 +73,5 @@ def getCharacterEncoding(word):
         word_embedding.append(get_char_vector(w))
     return word_embedding
 
-# getCharacterEncoding('ألزركش')
 
 
--- utils.py
+++ utils.py
@@ -5,12 +5,9 @@ import pickle
 import nltk
 import numpy as np
 from nltk.tokenize import sent_tokenize, word_tokenize
-nltk.download('punkt')
+# nltk.download('punkt')
 
 
-#convert using chr(harakat[0])
-harakat   = [1614,1615,1616,1618,1617,1611,1612,1613]
-connector = 1617
 
 
 def save_binary(data, file, folder):
@@ -22,36 +19,21 @@ def load_binary(file, folder):
     location  = os.path.join(folder, (file+'.pickle') )
     with open(location, 'rb') as ff:
         data = pickle.load(ff)
-
     return data
 
+harakat= load_binary('diacritics','./')
 
 
 
-# print(get_sentences("لله \nل \n والمنة"))
 
-def clear_punctuations(text):
-    text = "".join(c for c in text if c not in string.punctuation)
-    return text
-
-def clear_english_and_numbers(text):
-     text = re.sub(r"[a-zA-Z0-9٠-٩]", " ", text)
-     return text
-
-def is_tashkel(text):
-    return any(ord(ch) in harakat for ch in text)
 
 def clear_tashkel(text):
-    text = "".join(c for c in text if ord(c) not in harakat)
+    text = "".join(c for c in text if c not in harakat)
     return text
 
-def get_harakat():
-    return "".join(chr(item)+"|" for item in harakat)[:-1]
 
 
-# append _ if no tashekeel for the 7arf
-# otherwise, append the tashkeel itself 
-# when you loop, the haraka comes before the 7arf 
+
 
 def get_tashkel(sentence):
     output = []
@@ -61,16 +43,10 @@ def get_tashkel(sentence):
     while chIndex < (len(sentence)):
         characterTashkeels = []
         if mode == 1:
-            while chIndex < (len(sentence)) and ord(sentence[chIndex]) in harakat:
+            while chIndex < (len(sentence)) and sentence[chIndex] in harakat:
                 characterTashkeels.append(sentence[chIndex])
                 chIndex += 1
-            # else:
-            #     if current_haraka == "":
-            #         current_haraka = "_"
-            #     #print("7arf",current_haraka)
-            #     characterTashkeels.append(current_haraka)
-                # output.append(current_haraka)
-                # current_haraka = ""
+
             if (len(characterTashkeels) != 0):
                 output.append(characterTashkeels)
             else:
@@ -84,40 +60,15 @@ def get_tashkel(sentence):
     if mode == 1: # now I am exepcting tashkeel but the word ended before I find one
         output.append("_")  # _ symbolizes no tashkeel
     return output
-# def get_tashkel(sentence):
-#     output = []
-#     current_haraka = ""
-    
 
 
-#print(get_tashkel("اً,"))
-
-def combine_text_with_harakat(input_sent, output_sent):
-    #print("input : " , len(input_sent))
-  
-    #fix combine differences
-    input_length  = len(input_sent)
-    output_length = len(output_sent) # harakat_stack.size()
-    for index in range(0,(input_length-output_length)):
-        output_sent.append("")
-
-    #combine with text
-    text = ""
-    for character, haraka in zip(input_sent, output_sent):
-        if haraka == '<UNK>' or haraka == '_':
-            haraka = ''
-        text += character + "" + haraka
-
-    return text
 
 arabic_alphabet_set=load_binary('arabic_letters','./')
 arabic_alphabet = dict(zip(arabic_alphabet_set, list(range(len(arabic_alphabet_set)))))
-print(arabic_alphabet)
 
-print("length=")
-print(len(arabic_alphabet))
-#diacritic:id
-#harakat=load_binary('diacritic2id','./')
+
+
+
 
 
 def get_char_vector(char):
@@ -130,20 +81,3 @@ def get_char_vector(char):
     
 
 
-#harakat   = {1614:1,1615:2,1616:3,1618:4,1617:5,1611:6,1612:7,1613:8, 95:9}
-
-# def get_diacritic_hot_vector(haraka):
-#     if haraka not in harakat:
-#         return list(np.ones(15,dtype=int) )
-#     vector = [0 for _ in range(len(harakat))]
-#     vector[harakat[ord(haraka)] - 1] = 1
-#     return vector 
-
-# print(get_diacritic_hot_vector('ّ'))
-
-# print(arabic_alphabet.keys())
-
-# print(get_char_vector('ؤ'))
-
-
-# print(combine_text_with_harakat("ال",['_',""]))
